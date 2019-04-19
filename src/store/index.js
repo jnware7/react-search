09cb55cd48055a828e8e2a0abf68023fbb000975
jnware7 <jnware7@gmail.com>
2019-04-18 17:57:01 -0700

    jeff is at the bank

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 import reducer from '../reducer';
 
 const INITIAL_STATE = {
-  username: "Janny",
+  username: "Jeff",
   totalAmount: 2500701,
   previousAmount:2500701
 };
